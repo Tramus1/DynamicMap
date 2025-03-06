5eb463201825e5a94aab5bc2d47dff1ab7d3b6be
Tramus1 <115180948+Tramus1@users.noreply.github.com>
2025-03-05 18:31:38 -0600

    Update mapLogic.js

--- mapLogic.js
+++ mapLogic.js
@@ -31,9 +31,9 @@ function createDynamicSVG(point, sectorColor) {
     const isWarden = point.warden === "Prim" || point.warden === "Sec";
     const outerColor = isWarden
         ? point.warden === "Prim"
-            ? "clear"
-            : "clear"
-        : "clear";
+            ? "red"
+            : "yellow"
+        : "black";
     const wallColor = isWarden ? "white" : "black";
     const roofColor = isWarden ? "white" : "black";
 
@@ -42,8 +42,8 @@ function createDynamicSVG(point, sectorColor) {
         <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 16.933333 16.933333">
           <path id="OuterCircle" d="M 8.4666667,0.52916667 A 7.9375,7.9375 0 0 0 0.52916667,8.4666667 7.9375,7.9375 0 0 0 8.4666667,16.404167 7.9375,7.9375 0 0 0 16.404167,8.4666667 7.9375,7.9375 0 0 0 8.4666667,0.52916667 Z" style="fill:${outerColor};stroke-width:0.264583" />
           <circle id="InnerCircle" cx="8.4666672" cy="8.4666672" r="6.6145835" style="fill:${sectorColor};stroke-width:0.220486" />
-          <path id="Walls" d="M 5.0002116,8.0000285 V 13.00024 H 11.999784 V 8.0000285 Z" style="fill:${wallColor};stroke-width:0.264583" />
-          <path id="Roof" d="M 8.0000285,2.9998169 7.0000895,3.9997559 3.9997559,7.0000895 H 8.0000285 13.00024 L 9.9999064,3.9997559 8.9999674,2.9998169 H 8.5002563 Z" style="fill:${roofColor};stroke-width:0.264583" />
+          <path id="Walls" d="M 5.0002116,8.0000285 V 13.00024 H 11.999784 V 8.0000285 Z" style="fill:${sectorColor};stroke-width:0.264583" />
+          <path id="Roof" d="M 8.0000285,2.9998169 7.0000895,3.9997559 3.9997559,7.0000895 H 8.0000285 13.00024 L 9.9999064,3.9997559 8.9999674,2.9998169 H 8.5002563 Z" style="fill:${sectorColor};stroke-width:0.264583" />
         </svg>
     `;
     return svg;
@@ -56,7 +56,11 @@ points.forEach(point => {
     const sectorColor = sectorColors[point.sector] || "gray";
 
     // Create a custom icon using the dynamic SVG
-    
+    const icon = L.divIcon({
+        className: "custom-icon",
+        html: createDynamicSVG(point, sectorColor),
+        iconSize: [64, 64]
+    });
 
     // Add the marker to the map
     L.marker([point.lat, point.lon], { icon: icon })
