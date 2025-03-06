28b7c402762370688aea841a5227a6a06b366823
Tramus1 <115180948+Tramus1@users.noreply.github.com>
2025-03-05 18:41:33 -0600

    Update mapLogic.js

--- mapLogic.js
+++ mapLogic.js
@@ -66,6 +66,6 @@ points.forEach(point => {
     L.marker([point.lat, point.lon], { icon: icon })
         .addTo(map)
         .bindPopup(
-            `<b>${point.name}</b><br>Sector: ${point.sector}<br>`
+            `<b>${point.name}</b>`
         );
 });
